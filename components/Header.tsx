8295e27a884d8c56dcf15523cc5b8bcc8badc840
Pauloplanob1402 <planob1402@gmail.com>
2026-09-03 19:13:53 -0300

    Adiciona logo DUOS no cabecalho e favicon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,11 @@
+import Image from 'next/image'
+import logoDuos from '@/public/logo-duos.png'
+
 export default function Header() {
   return (
     <header className="relative z-20">
       <div className="max-w-6xl mx-auto px-6 py-5">
-        <span className="font-display font-extrabold text-xl tracking-tight text-white">DUOS</span>
+        <Image src={logoDuos} alt="DUOS" priority className="h-7 w-auto" />
       </div>
     </header>
   )
